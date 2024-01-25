a2eab08ad9df9eec31a47ad9d59d8f7d3dbffa50
Adibe Nnamdi <adibennamdisolomon@gmail.com>
2024-01-25 13:04:11 +0100

    Added a loader

--- valencia.js
+++ valencia.js
@@ -1,1 +1,10 @@
 document.getElementById('val').innerHTML="Welcome & Greetings!";
+
+let loader = document.querySelector('.loader');
+let pacman = document.getElementById('pacman');
+
+window.onload = function(){
+    loader.style.display='none';
+
+    pacman.style.display='block';
+}
